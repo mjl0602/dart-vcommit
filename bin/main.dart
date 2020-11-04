15cae7e9cc20adee9eb92432cae127bac90232d7
MJL <675531696@qq.com>
2020-11-04 11:22:10 +0800

    update

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,15 @@
 import 'dart:io';
 
 import 'package:args/args.dart';
+import 'package:path/path.dart' as path;
 
-var markList = [
+/// 脚本存放路径
+// Uri templatePath = Platform.script.resolve('../template/');
+
+/// 脚本运行路径
+Uri shellPath = Uri.parse(path.join(Platform.environment['PWD']));
+
+var markInfo = [
   "feat",
   "fix",
   "docs",
@@ -30,10 +37,9 @@ main(List<String> args) {
       negatable: false,
       help: "查看指令帮助",
     );
-  for (var mark in markList) {
+  for (var mark in markInfo) {
     argParser.addFlag(
       mark,
-      // abbr: 'u',
       negatable: false,
       help: "添加标记:$mark",
     );
@@ -47,30 +53,63 @@ main(List<String> args) {
   var command = _argResults.arguments.first;
   print('执行命令:$command');
   print(args);
-
-  String version = "1.2.2";
+  Project project = Project.currentPath();
+  if (project == null) {
+    print('不支持当前项目格式');
+    return;
+  }
+  String version = project.currentVersion;
   String mark = "Fixed";
   String content = 'init commit';
-  var res = Process.runSync(
-    "git",
-    ["commit", "-m", "'[$version]$mark: $content'"],
-    runInShell: true,
-  );
-  print(res.exitCode);
+  print('$version');
+  // var res = Process.runSync(
+  //   "git",
+  //   ["commit", "-m", "'[$version]$mark: $content'"],
+  //   runInShell: true,
+  // );
+  // print(res.exitCode);
+  // print("Err: ${res.stderr}");
+  // print('Out: ${res.stdout}');
 }
 
 enum ProjectType {
   unknown,
-  js,
+  node,
   spring,
-  flutter,
+  pub,
   ios,
   android,
 }
 
+Map<String, ProjectType> typeMap = {
+  "pubspec.yaml": ProjectType.pub,
+  "package.json": ProjectType.node,
+};
+
 /// 当前项目
 class Project {
   final String currentVersion;
+  final ProjectType type;
+  Project._(this.currentVersion, this.type);
 
-  Project(this.currentVersion);
+  /// 读取当前目录文件，判断项目类型
+  /// 如果没有任何匹配，返回null
+  factory Project.currentPath() {
+    String currentVersion;
+    ProjectType type;
+    print('shellPath:$shellPath');
+    var dir = Directory.fromUri(shellPath);
+    for (var file in dir.listSync()) {
+      var name = path.basename(file.path);
+      type = typeMap[name];
+      if (type != null) {
+        break;
+      }
+    }
+    currentVersion = "$type";
+    if (type == null) {
+      return null;
+    }
+    return Project._(currentVersion, type);
+  }
 }
